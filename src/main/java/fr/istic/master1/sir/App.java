27f282bac479e920b09e3311d53e667300ae1e6d
JulienMoulinSIR <j.moulin85470@gmail.com>
2015-04-24 12:03:19 +0200

    add TP1 - Introduction Maven

---
+++ src/main/java/fr/istic/master1/sir/App.java
@@ -0,0 +1,11 @@
+package fr.istic.master1.sir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
